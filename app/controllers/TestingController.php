485b41ade0b03cfa5315c46e52ec43dff7dc5651
Anomali27 <edwardcornelius27@gmail.com>
2026-03-04 09:01:16 +0700

    connect github

---
+++ app/controllers/TestingController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controllers;
+
+class TestingController
+{
+    public function testing()
+    {
+        require_once '../app/views/testing/index.php';
+    }
+}
+
+?>
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../app/core/Router.php';
+
+use App\Core\Router;
+
+$router = new Router();
+
+//Register Routes
+$router->add('GET','/testing', 'TestingController', 'testing');
+
+
+$router->run();
+?>
